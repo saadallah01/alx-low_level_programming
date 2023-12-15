3276a6215273ddf184850348322801c287ed0fb8
saadallah01 <saadallah.kaddouri01@gmail.com>
2023-12-14 16:05:13 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bash/bin
+#include <stdio.h>
+/**
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
+
